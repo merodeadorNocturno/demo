2c135e0fff4ee0838d2335ab44a91a687858a95a
merodeadorNocturno <merodeador.nocturno@gmail.com>
2023-06-02 21:08:22 -0600

    Add GET controller endpoint for classrooms

--- src/classrooms/classrooms.controller.ts
+++ src/classrooms/classrooms.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Controller('classrooms')
-export class ClassroomsController {}
+export class ClassroomsController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getClassrooms(): string {
+    return this.appService.getClassrooms();
+  }
+}
