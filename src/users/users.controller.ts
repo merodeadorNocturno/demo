c1421a4401754310831133515b86e32adeecf953
merodeadorNocturno <merodeador.nocturno@gmail.com>
2023-06-02 21:52:40 -0600

    Fix a bug that required Users to be a string

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from 'src/app.service';
+import { Users } from 'src/types';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getUsers(): string {
+  getUsers(): Users {
     return this.appService.getUsers();
   }
 }
