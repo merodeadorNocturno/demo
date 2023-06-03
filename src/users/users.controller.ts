5eab413802184b5f585937b3764277d43746999e
merodeadorNocturno <merodeador.nocturno@gmail.com>
2023-06-02 19:25:36 -0600

    Added Get method for users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from 'src/app.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getUsers(): string {
+    return this.appService.getUsers();
+  }
+}
